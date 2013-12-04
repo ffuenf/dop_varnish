4076c1387d9fc69be0893ff3cb735ff6ed085db1
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-12-04 13:22:48 +0100

    fix apt-repo ?

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,13 @@
 # Recipe:: default
 #
 
-include_recipe "varnish::apt_repo"
+apt_repository "varnish-cache" do
+  uri "http://repo.varnish-cache.org/#{node['platform']}"
+  distribution node['lsb']['codename']
+  components ["varnish-#{node['varnish']['version']}"]
+  key "http://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
+end
+
 include_recipe "varnish"
 
 # create secret file
