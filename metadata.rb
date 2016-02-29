bdacb13ca2a500ec283b88f6fd9113613d66ec62
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-02-29 11:15:04 +0100

    fix foodcritic

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ description 'installs/configures dop_varnish'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name 'dop_varnish'
 version '2.2.1'
+issues_url 'https://github.com/ffuenf/dop_varnish/issues'
+source_url 'https://github.com/ffuenf/dop_varnish'
 
 %w(debian ubuntu).each do |os|
   supports os
